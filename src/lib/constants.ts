2c2c23900d249ce8691cc7c4cd2224c9a61c7ebe
Henrique Gomes <hikkgomes@gmail.com>
2025-11-03 11:30:47 +0000

    Can you change the expiry time to 1 min just for testing purposes?

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "InvoiceLink";
-export const QUOTE_EXPIRY_MS = 5 * 60 * 1000; // 5 minutes
+export const QUOTE_EXPIRY_MS = 1 * 60 * 1000; // 1 minute
 export const JWT_SECRET = process.env.JWT_SECRET || "default-secret-key-that-is-not-very-secret";
 export const MEMPOOL_API_URL = "https://mempool.space/api";
